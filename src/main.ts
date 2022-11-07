effeec4feec618a76fb4df9d41ec39f5e7155677
Eduardo Russo <russoedu@gmail.com>
2022-11-07 08:01:44 +0000

    Prepared

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import p5 from 'p5';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import P5 from 'p5'
+import './style.css'
 
-import './style.css';
+const _app = new P5(p5Instance => {
+  const p = p5Instance as unknown as P5
 
-const _app = new p5(p5Instance => {
-  const p = p5Instance as unknown as p5;
+  const x = 100
+  const y = 100
 
-  const x = 100;
-  const y = 100;
+  p.setup = function setup () {
+    p.createCanvas(500, 500)
+  }
 
-  p.setup = function setup() {
-    p.createCanvas(500, 500);
-  };
-
-  p.draw = function draw() {
-    p.background(0);
-    p.fill(205);
-    p.rect(x, y, 50, 50);
-  };
-}, document.getElementById('app')!);
+  p.draw = function draw () {
+    p.background(0)
+    p.fill(205)
+    p.rect(x, y, 50, 50)
+  }
+}, document.getElementById('app')!)
